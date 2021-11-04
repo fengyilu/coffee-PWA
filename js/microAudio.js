d3b8c57d17e233e55521d1e36035178298b5d23a
fengyilu <fengyilu1998@gmail.com>
2021-11-04 18:10:52 +0100

    sad

--- js/microAudio.js
+++ js/microAudio.js
@@ -82,7 +82,7 @@ function playVideo() { // as blob
 
     var video = document.getElementById('video');
 
-    var videoUrl = window.URL.createObjectURL(blobs[0]);// blob.data gives actual data
+    var videoUrl = window.URL.createObjectURL(localstorage.getItem("file"));// blob.data gives actual data
 
     video.src = videoUrl;
 }
