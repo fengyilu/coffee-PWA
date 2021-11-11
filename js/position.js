86e0ebadabd26c63d9e2fc1e967a63ec9666ac15
fengyilu <fengyilu1998@gmail.com>
2021-11-11 17:06:41 +0100

    we

--- js/position.js
+++ js/position.js
@@ -13,7 +13,6 @@ function deviceOrientationHandler (eventData) {
   document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
   document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
   document.getElementById("doDirection").innerHTML = Math.round(dir);
-
   var logo = document.getElementById("imgLogo");
   logo.style.webkitTransform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
   logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
