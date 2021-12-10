7a869569861996f8c73995dd3d3e1a642f69f9d0
fengyilu <fengyilu1998@gmail.com>
2021-12-10 09:15:41 +0100

    asdqwe

--- serviceWorker.js
+++ serviceWorker.js
@@ -51,3 +51,21 @@ const latest = {
       })
     );
   });
+
+  // Activate service worker
+self.addEventListener('activate', function(event) {
+  console.log("Service Worker activate")
+  event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+          return Promise.all(
+              cacheNames.filter(function(cacheName) {
+                  // Return true if you want to remove this cache,
+                  // but remember that caches are shared across
+                  // the whole origin
+              }).map(function(cacheName) {
+                  return caches.delete(cacheName);
+              })
+          );
+      })
+  );
+});
